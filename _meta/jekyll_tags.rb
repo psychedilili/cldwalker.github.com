1caddda45e95b324edbfa22f7c16c892cb8983f6
Gabriel Horner <gabriel.horner@gmail.com>
2009-02-18 21:33:05 -0500

    split off tag helper methods from main thor tasks, added new post template

---
+++ _meta/jekyll_tags.rb
@@ -0,0 +1,40 @@
+# helper methods used mainly for my Jekyll tags
+module JekyllTags
+  def all_tags
+    tag_count.keys
+  end
+
+  def posts_dir
+   File.join(root_dir, '_posts')
+  end
+
+  #Returns all posts under _posts
+  def posts
+    Dir["#{posts_dir}/**"].select {|e| e !~ /~$/ }
+  end
+
+  #array of posts with their tags
+  def post_tags
+    posts.map do |e|
+      yaml = YAML::load_file(e)
+      [e, yaml['tags']]
+    end
+  end
+
+  #array of tags with their counts
+  def tag_count
+    count = {}
+    post_tags.each do |post, tags|
+      tags.each do |t|
+        count[t] ||= 0
+        count[t] += 1
+      end
+    end
+    count
+  end
+
+  # Use this method to print the output of any methods returning data structures
+  def print_method(method)
+    p send(method)
+  end
+end
