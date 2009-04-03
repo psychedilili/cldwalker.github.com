75c3cf8e70cdf1b8db5a9d384be1593f4754ff7e
Gabriel Horner <gabriel.horner@gmail.com>
2009-04-03 15:26:06 -0400

    proper indentation + change localhost to tagaholic

--- javascripts/jquery.githubSort.js
+++ javascripts/jquery.githubSort.js
@@ -30,74 +30,75 @@
     $("ul.projects").before("<div id='bookmarklet_status' style='text-align:center;padding:2px;margin-top:20px;border:1px solid #D8D8D8;\
       background-color: #F0F0F0;'>Loading sort for "+github_user+"...<img src='http://github.com/images/modules/ajax/indicator.gif'/></div>");
   };
-function detect(array, callback) {
-  return $.grep(array,callback)[0];
-};
 
-function addRepo(repo_div, json_repo) {
-  repo_div.before("<span style='float:right; color:#4183C4'><span class='watchers_num'>" + json_repo.watchers + "</span> watchers<br/>" + 
-    "<span class='forks_num'>" + json_repo.forks + "</span> forks" + '</span>');
-};
+  function detect(array, callback) {
+    return $.grep(array,callback)[0];
+  };
 
-function loadRepoStats(json) {
-  $('div.title').each(function() {
-    var repo_div = $(this);
-    var repo_obj = detect(json.user.repositories, function(e) { 
-      return e.url.match( new RegExp(repo_div.text()+ '$') ) 
+  function addRepo(repo_div, json_repo) {
+    repo_div.before("<span style='float:right; color:#4183C4'><span class='watchers_num'>" + json_repo.watchers + "</span> watchers<br/>" + 
+      "<span class='forks_num'>" + json_repo.forks + "</span> forks" + '</span>');
+  };
+
+  function loadRepoStats(json) {
+    $('div.title').each(function() {
+      var repo_div = $(this);
+      var repo_obj = detect(json.user.repositories, function(e) { 
+        return e.url.match( new RegExp(repo_div.text()+ '$') ) 
+      });
+      if (repo_obj) {addRepo(repo_div, repo_obj);}
     });
-    if (repo_obj) {addRepo(repo_div, repo_obj);}
-  });
-};
+  };
 
-function sourceSortPlugin() {
-  var _s = document.createElement('script');
-  _s.type='text/javascript';
-  // original source: 'http://plugins.jquery.com/files/jquery.selso-1.0.1.js.txt'
-  _s.src='http://localhost:4000/javascripts/jquery.selso.js';
-  document.getElementsByTagName('head')[0].appendChild(_s);
-};
+  function sourceSortPlugin() {
+    var _s = document.createElement('script');
+    _s.type='text/javascript';
+    // original source: 'http://plugins.jquery.com/files/jquery.selso-1.0.1.js.txt'
+    _s.src='http://tagaholic.me/javascripts/jquery.selso.js';
+    document.getElementsByTagName('head')[0].appendChild(_s);
+  };
 
-function createSortBox() {
-  $('#bookmarklet_status').replaceWith("\
-  <style type='text/css'>\
-    .desc_sort {\
-      background: url(http://localhost:4000/images/arrow-down.gif) no-repeat 0 center;\
-      padding: 0 10px;\
-    }\
-    .asc_sort {\
-      background: url(http://localhost:4000/images/arrow-up.gif) no-repeat 0 center;\
-      padding: 0 10px;\
-    }\
-    .sort_label {\
-      font-weight: bold;\
-      color:#4183C4;\
-    }\
-    #sort_links {\
-      text-align:center;\
-      padding:2px;\
-      margin-top:20px;\
-      border:1px solid #D8D8D8;\
-      background-color: #F0F0F0;\
-    }\
-    #sort_links_label {\
-      font-style: italic;\
-      font-size: 120%;\
-      padding-right:10px;\
-      color:#888;\
-    }\
-  </style>\
-  <div id='sort_links'>\
-    <span id='sort_links_label'>Sort Repositories:</span>\
-    <span class='sort_label'>NAME</span>\
-    <a class='name_sort asc_sort' style='display:none' href=\"javascript:$.githubRepoSort('name', 'asc')\"></a>\
-    <a class='name_sort desc_sort' href=\"javascript:$.githubRepoSort('name', 'desc')\"></a>\
-    | <span class='sort_label'>WATCHERS</span>\
-    <a class='watchers_sort asc_sort' style='display:none' href=\"javascript:$.githubRepoSort('watchers', 'asc')\"></a>\
-    <a class='watchers_sort desc_sort' href=\"javascript:$.githubRepoSort('watchers', 'desc')\"></a>\
-    | <span class='sort_label'>FORKS</span>\
-    <a class='forks_sort asc_sort' style='display:none' href=\"javascript:$.githubRepoSort('forks', 'asc')\"></a>\
-    <a class='forks_sort desc_sort' href=\"javascript:$.githubRepoSort('forks', 'desc')\"></a>\
-  </div>\
-    ");
-};
+  function createSortBox() {
+    $('#bookmarklet_status').replaceWith("\
+    <style type='text/css'>\
+      .desc_sort {\
+        background: url(http://tagaholic.me/images/arrow-down.gif) no-repeat 0 center;\
+        padding: 0 10px;\
+      }\
+      .asc_sort {\
+        background: url(http://tagaholic.me/images/arrow-up.gif) no-repeat 0 center;\
+        padding: 0 10px;\
+      }\
+      .sort_label {\
+        font-weight: bold;\
+        color:#4183C4;\
+      }\
+      #sort_links {\
+        text-align:center;\
+        padding:2px;\
+        margin-top:20px;\
+        border:1px solid #D8D8D8;\
+        background-color: #F0F0F0;\
+      }\
+      #sort_links_label {\
+        font-style: italic;\
+        font-size: 120%;\
+        padding-right:10px;\
+        color:#888;\
+      }\
+    </style>\
+    <div id='sort_links'>\
+      <span id='sort_links_label'>Sort Repositories:</span>\
+      <span class='sort_label'>NAME</span>\
+      <a class='name_sort asc_sort' style='display:none' href=\"javascript:$.githubRepoSort('name', 'asc')\"></a>\
+      <a class='name_sort desc_sort' href=\"javascript:$.githubRepoSort('name', 'desc')\"></a>\
+      | <span class='sort_label'>WATCHERS</span>\
+      <a class='watchers_sort asc_sort' style='display:none' href=\"javascript:$.githubRepoSort('watchers', 'asc')\"></a>\
+      <a class='watchers_sort desc_sort' href=\"javascript:$.githubRepoSort('watchers', 'desc')\"></a>\
+      | <span class='sort_label'>FORKS</span>\
+      <a class='forks_sort asc_sort' style='display:none' href=\"javascript:$.githubRepoSort('forks', 'asc')\"></a>\
+      <a class='forks_sort desc_sort' href=\"javascript:$.githubRepoSort('forks', 'desc')\"></a>\
+    </div>\
+      ");
+  };
 })(jQuery);
